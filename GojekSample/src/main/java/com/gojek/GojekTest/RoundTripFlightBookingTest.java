39008a3425ad0d37a9e496f382b629067ed19a7f
abhishek bisht <abhishek.annibisht@gmail.com>
2017-02-09 18:23:51 +0530

    Added test class

---
+++ GojekSample/src/main/java/com/gojek/GojekTest/RoundTripFlightBookingTest.java
@@ -0,0 +1,31 @@
+package com.gojek.GojekTest;
+
+import java.io.IOException;
+import java.text.ParseException;
+
+import org.apache.poi.hssf.extractor.ExcelExtractor;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+import com.gojek.GojekHelper.ExcelReader;
+import com.gojek.GojekLogic.RoundTripFlightBooking;
+
+public class RoundTripFlightBookingTest extends RoundTripFlightBooking {
+	ExcelReader read = new ExcelReader();
+	
+  @Test(description="flight Booking Test" , dataProvider="flight")
+  public void roundtripbookingtest(String url,String fromcity , String tocity, String firstname , String lastname,String emailid) throws ParseException 
+  {
+	 roundtripflight(url, fromcity, tocity, firstname, lastname, emailid);
+	 
+  }
+  
+  @DataProvider(name = "flight")
+ 	public Object[][] dataProvideremail() throws IOException {
+
+ 		read.testDataFile(File);
+ 		Object[][] data = new Object[][] {{  read.readFromColumn("Base", 1, 5)} };
+ 		read.closeFile();
+ 		return data;
+ 	}
+}
