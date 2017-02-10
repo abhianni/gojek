719c3f3ea25d3bd3d08ce8be1937a6913db21873
abhishek bisht <abhishek.annibisht@gmail.com>
2017-02-10 08:11:23 +0530

    Updated few values

--- GojekSample/src/main/java/com/gojek/GojekTest/RoundTripFlightBookingTest.java
+++ GojekSample/src/main/java/com/gojek/GojekTest/RoundTripFlightBookingTest.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.text.ParseException;
 
 import org.apache.poi.hssf.extractor.ExcelExtractor;
+import org.testng.Assert;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
@@ -12,20 +13,28 @@ import com.gojek.GojekLogic.RoundTripFlightBooking;
 
 public class RoundTripFlightBookingTest extends RoundTripFlightBooking {
 	ExcelReader read = new ExcelReader();
-	
-  @Test(description="flight Booking Test" , dataProvider="flight")
-  public void roundtripbookingtest(String url,String fromcity , String tocity, String firstname , String lastname,String emailid) throws ParseException 
-  {
-	 roundtripflight(url, fromcity, tocity, firstname, lastname, emailid);
-	 
-  }
-  
-  @DataProvider(name = "flight")
- 	public Object[][] dataProvideremail() throws IOException {
 
- 		read.testDataFile(File);
- 		Object[][] data = new Object[][] {{  read.readFromColumn("Base", 1, 0),read.readFromColumn("Base", 1, 1),read.readFromColumn("Base", 1, 2),read.readFromColumn("Base", 1, 4),read.readFromColumn("Base", 1, 5),read.readFromColumn("Base", 1, 3)} };
- 		read.closeFile();
- 		return data;
- 	}
+	@Test(description = "flight Booking Test", dataProvider = "flight")
+	public void roundtripbookingtestPass(String url, String fromcity, String tocity, String firstname, String lastname,
+			String emailid) throws ParseException {
+		roundtripflight(url, fromcity, tocity, firstname, lastname, emailid);
+		Assert.assertNotNull(element(locator("makePaymentButton")));
+	}
+
+	@Test(description = "flight Booking Test", dataProvider = "flight")
+	public void roundtripbookingtestFail(String url, String fromcity, String tocity, String firstname, String lastname,
+			String emailid) throws ParseException {
+		roundtripflight(url, fromcity, tocity, firstname, lastname, emailid);
+		Assert.assertNull(element(locator("makePaymentButton")));
+	}
+	@DataProvider(name = "flight")
+	public Object[][] dataProvideremail() throws IOException {
+
+		read.testDataFile(File);
+		Object[][] data = new Object[][] { { read.readFromColumn("Base", 1, 0), read.readFromColumn("Base", 1, 1),
+				read.readFromColumn("Base", 1, 2), read.readFromColumn("Base", 1, 4), read.readFromColumn("Base", 1, 5),
+				read.readFromColumn("Base", 1, 3) } };
+		read.closeFile();
+		return data;
+	}
 }
